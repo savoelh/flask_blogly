425fcfdf91d36a5722c18c77cf4a8ae7caf78a44
root <root@DESKTOP-ORJC91E.localdomain>
2022-08-11 07:20:25 -0600

    jkahfjdasf

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
-
+import datetime
 db = SQLAlchemy()
 
 def connect_db(app):
@@ -11,13 +12,38 @@ class Users(db.Model):
 
     __tablename__ = 'users'
 
-    # @classmethod
-    # def get_all_hungry(cls):
-    #     return cls.query.filter(Pet.hunger > 20).all()
-    # def  __repr__(self):
-    #     return f'<Pet id ={self.id}'
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(10), nullable=False)
     last_name = db.Column(db.String(10), nullable=False)
     image_url = db.Column(db.String(200), nullable = True)
+
+    @property
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Posts(db.Model):
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    created_at =  db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    users = db.relationship('Users', backref='posts')
+
+class Tag(db.Model):
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    name = db.Column(db.String, unique=True)
+
+class PostTag(db.Model):
+
+    __tablename__ = 'posttags'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), nullable=False)
--- seed.py
+++ seed.py
@@ -1,8 +1,14 @@
-from models import Users, db
+from models import Users, Posts, Tag, PostTag, db
 from app import app
 
 db.drop_all()
 db.create_all()
 
 Users.query.delete()
+Posts.query.delete()
+Tag.query.delete()
+PostTag.query.delete()
 
+db.session.add()
+
+db.session.commit()
